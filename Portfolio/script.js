44915181e7867e5e731d41569f1650f4e1f06950
vijendra2244 <vs230267@gmail.com>
2023-12-04 14:16:12 +0530

    Portfolio

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,9 +1,9 @@
 var hamburgerBtn = document.getElementById("btnOfHamburger");
-var ulListSecond = document.getElementsByClassName("ulListSecond")[0];
+var ulListSecond = document.getElementsByClassName("ulList")[0];
 
-hamburgerBtn.addEventListener("click", function() {
+hamburgerBtn.addEventListener("click", function () {
   if (window.innerWidth <= 640) {
     hamburgerBtn.classList.toggle("active");
-    ulListSecond.classList.toggle("ulListSecond");
+    ulListSecond.classList.toggle("hide");
   }
 });
